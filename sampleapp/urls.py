48e2306c3c3fe465bd27d34ac58843eb96e4f5db
Ananthu Cinil <ananthuckofficial@gmail.com>
2024-05-29 14:40:17 +0530

    secondcommit

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     # path('',views.phello,name='phello'),
     path('',views.index, name="index"),
     path('loginpage2',views.loginpage2, name= 'loginpage2'),
+    path('contact_mail',views.contact_mail, name= 'contact_mail'),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here
 # def phello(request):
@@ -10,3 +12,20 @@ def index(request):
 
 def loginpage2(request):
     return render(request,'loginpage2.html')
+
+@csrf_exempt
+def contact_mail(request):
+    if request.method == 'POST':
+        name=request.POST.get('Name')
+        email=request.POST.get('Email')
+        message=request.POST.get('Message')
+        print(name, email, message)
+        send_mail(
+            subject=f"New Contact Form Submission from {name}",
+            message=message,
+            from_email='noreply@example.com',
+            recipient_list=['ananthuckofficial@gmail.com'],
+        )
+        
+        return redirect('index')
+    return render(request, 'Index.html')
